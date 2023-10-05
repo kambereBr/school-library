6a2c1a3d380bfeb5b50858e47d3826566f936ff9
kambereBr <brunokambere@gmail.com>
2023-10-05 23:08:10 +0200

    Create the many-to-many (also has-many-through) relationship between Person and Book

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 require_relative 'nameable'
 
 class Person < Nameable
-  attr_reader :id
+  attr_reader :id, :rental
   attr_accessor :age, :name
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -10,6 +10,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rental = []
   end
 
   def can_use_services?
