55a7ae6ee07e5dadb6fda66296080653a7e652eb
JavierAybar <129561152+JavierAybar@users.noreply.github.com>
2023-10-12 15:24:49 -0300

    Fix linters

--- app/spec/capitalize_spec.rb
+++ app/spec/capitalize_spec.rb
@@ -1,10 +1,10 @@
 require_relative '../models/capitalize_decorator'
 
 describe '#CapitalizeDecorator' do
-  let(:person) { Person.new(22, 'javier') }
+  let(:person) { Person.new(22, 'javiera') }
   let(:new_capitalize) { CapitalizeDecorator.new(person) }
 
   it 'correct_name should to capitalize name' do
-    expect(new_capitalize.correct_name).to eq('Javier')
+    expect(new_capitalize.correct_name).to eq('Javiera')
   end
 end
