6f338d9200dfb60653583fdab533fcd6f5dbe6da
kambereBr <brunokambere@gmail.com>
2023-10-12 19:57:13 +0200

    Fix inters error

--- app/spec/decorator_spec.rb
+++ app/spec/decorator_spec.rb
@@ -2,13 +2,13 @@ require_relative '../models/decorator'
 require_relative '../models/nameable'
 
 describe '#Decorator' do
-    let(:new_nameable) {double('Nameable', correct_name: 'Javier')}
-    let(:new_decorator) {Decorator.new(new_nameable)}
-    it 'Should initialize new Decorator instance with nameable property' do
-        expect(new_decorator.nameable).to eq(new_nameable)
-    end
+  let(:new_nameable) { double('Nameable', correct_name: 'Javier') }
+  let(:new_decorator) { Decorator.new(new_nameable) }
+  it 'Should initialize new Decorator instance with nameable property' do
+    expect(new_decorator.nameable).to eq(new_nameable)
+  end
 
-    it 'correct_name should return Javier' do
-        expect(new_decorator.correct_name).to eq('Javier')
-    end
+  it 'correct_name should return Javier' do
+    expect(new_decorator.correct_name).to eq('Javier')
+  end
 end
--- app/spec/trimmer_decorator_spec.rb
+++ app/spec/trimmer_decorator_spec.rb
@@ -1,9 +1,9 @@
 require_relative '../models/trimmer_decorator'
 
 describe '#TrimmerDecorator' do
-    let(:person) {Person.new(22, 'JavierAybar')}
-    let(:new_trimmer) {TrimmerDecorator.new(person)}
-    it 'Should trim JavierAybar and return JavierAyba' do
-        expect(new_trimmer.correct_name).to eq('JavierAyba')
-    end
+  let(:person) { Person.new(22, 'JavierAybar') }
+  let(:new_trimmer) { TrimmerDecorator.new(person) }
+  it 'Should trim JavierAybar and return JavierAyba' do
+    expect(new_trimmer.correct_name).to eq('JavierAyba')
+  end
 end
