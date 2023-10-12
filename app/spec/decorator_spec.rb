e8f84803d1ae65bafb483b26def44db2ec6a61bd
kambereBr <brunokambere@gmail.com>
2023-10-12 19:55:14 +0200

    Add Decorator unit tests

---
+++ app/spec/decorator_spec.rb
@@ -0,0 +1,14 @@
+require_relative '../models/decorator'
+require_relative '../models/nameable'
+
+describe '#Decorator' do
+    let(:new_nameable) {double('Nameable', correct_name: 'Javier')}
+    let(:new_decorator) {Decorator.new(new_nameable)}
+    it 'Should initialize new Decorator instance with nameable property' do
+        expect(new_decorator.nameable).to eq(new_nameable)
+    end
+
+    it 'correct_name should return Javier' do
+        expect(new_decorator.correct_name).to eq('Javier')
+    end
+end
